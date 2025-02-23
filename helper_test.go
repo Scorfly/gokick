3e867e14c78c0089d951746dc2f0723e4b9f26b7
Scorfly <scorfly@gmail.com>
2025-02-23 01:09:14 +0100

    feat: add endpoint to refresh token

--- helper_test.go
+++ helper_test.go
@@ -26,12 +26,41 @@ func setupMockClient(t *testing.T, mockHandler http.HandlerFunc) *gokick.Client 
 	return kickClient
 }
 
+func setupMockAuthClient(t *testing.T, mockHandler http.HandlerFunc) *gokick.Client {
+	t.Helper()
+
+	server := httptest.NewServer(mockHandler)
+	kickClient, err := gokick.NewClient(&gokick.ClientOptions{
+		ClientID:     "client-id",
+		ClientSecret: "client-secret",
+		AuthBaseURL:  fmt.Sprintf("http://%s", server.Listener.Addr()),
+	})
+	require.NoError(t, err)
+
+	t.Cleanup(func() { server.Close() })
+
+	return kickClient
+}
+
 func setupTimeoutMockClient(t *testing.T) *gokick.Client {
 	t.Helper()
 
 	kickClient, err := gokick.NewClient(&gokick.ClientOptions{
 		UserAccessToken: "access-token",
 		HTTPClient:      &http.Client{Timeout: 1 * time.Nanosecond},
+	})
+	require.NoError(t, err)
+
+	return kickClient
+}
+
+func setupTimeoutMockAuthClient(t *testing.T) *gokick.Client {
+	t.Helper()
+
+	kickClient, err := gokick.NewClient(&gokick.ClientOptions{
+		ClientID:     "client-id",
+		ClientSecret: "client-secret",
+		HTTPClient:   &http.Client{Timeout: 1 * time.Nanosecond},
 	})
 	require.NoError(t, err)
 
