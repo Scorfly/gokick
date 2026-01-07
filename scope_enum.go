ffb48730045ea5afa3fc37b1652b429ac08ebd0c
Louis Marie <scorfly@gmail.com>
2026-01-07 17:31:11 +0100

    Update scopes: add rewards and moderation chat message scopes, fix streamkey typo (#80)

--- scope_enum.go
+++ scope_enum.go
@@ -5,14 +5,17 @@ import "fmt"
 type Scope int
 
 const (
-	ScopeUserRead       Scope = iota // user:read
-	ScopeChannelRead                 // channel:read
-	ScopeChannelWrite                // channel:write
-	ScopeChatWrite                   // chat:write
-	ScopeStremkeyRead                // streamkey:read
-	ScopeEventSubscribe              // events:subscribe
-	ScopeModerationBan               // moderation:ban
-	ScopeKicksRead                   // kicks:read
+	ScopeUserRead                    Scope = iota // user:read
+	ScopeChannelRead                              // channel:read
+	ScopeChannelWrite                             // channel:write
+	ScopeChannelRewardsRead                       // channel:rewards:read
+	ScopeChannelRewardsWrite                      // channel:rewards:write
+	ScopeChatWrite                                // chat:write
+	ScopeStreamkeyRead                            // streamkey:read
+	ScopeEventSubscribe                           // events:subscribe
+	ScopeModerationBan                            // moderation:ban
+	ScopeModerationChatMessageManage              // moderation:chat_message:manage
+	ScopeKicksRead                                // kicks:read
 )
 
 func NewScope(scope string) (Scope, error) {
@@ -23,14 +26,20 @@ func NewScope(scope string) (Scope, error) {
 		return ScopeChannelRead, nil
 	case "channel:write":
 		return ScopeChannelWrite, nil
+	case "channel:rewards:read":
+		return ScopeChannelRewardsRead, nil
+	case "channel:rewards:write":
+		return ScopeChannelRewardsWrite, nil
 	case "chat:write":
 		return ScopeChatWrite, nil
 	case "streamkey:read":
-		return ScopeStremkeyRead, nil
+		return ScopeStreamkeyRead, nil
 	case "events:subscribe":
 		return ScopeEventSubscribe, nil
 	case "moderation:ban":
 		return ScopeModerationBan, nil
+	case "moderation:chat_message:manage":
+		return ScopeModerationChatMessageManage, nil
 	case "kicks:read":
 		return ScopeKicksRead, nil
 	default:
@@ -46,14 +55,20 @@ func (s Scope) String() string {
 		return "channel:read"
 	case ScopeChannelWrite:
 		return "channel:write"
+	case ScopeChannelRewardsRead:
+		return "channel:rewards:read"
+	case ScopeChannelRewardsWrite:
+		return "channel:rewards:write"
 	case ScopeChatWrite:
 		return "chat:write"
-	case ScopeStremkeyRead:
+	case ScopeStreamkeyRead:
 		return "streamkey:read"
 	case ScopeEventSubscribe:
 		return "events:subscribe"
 	case ScopeModerationBan:
 		return "moderation:ban"
+	case ScopeModerationChatMessageManage:
+		return "moderation:chat_message:manage"
 	case ScopeKicksRead:
 		return "kicks:read"
 	default:
